a2e5e5a38f623a3de84a7043efd3a882633f5b90
Guilherme Lemes <guilhermelemes08@gmail.com>
2024-11-09 14:51:38 -0300

    Primeiro commit: Configuração inicial do projeto

---
+++ src/main/java/br/com/backend/solidariedade_na_mesa/SolidariedadeNaMesaApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.solidariedade_na_mesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidariedadeNaMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidariedadeNaMesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backend/solidariedade_na_mesa/SolidariedadeNaMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.solidariedade_na_mesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidariedadeNaMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
